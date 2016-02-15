bfc46d9f78ff493d7045bdbd47f8b06dc95fcd4a
Joanna Soh <joannahsoh@gmail.com>
2016-02-15 12:53:44 -0700

    Sphere

---
+++ Term3/JSShapes/Sphere.py
@@ -0,0 +1,11 @@
+class Sphere():
+	def __init__(self,diameter,radius,pi):
+		self.diameter = diameter
+		self.radius = radius
+		self.pi = math.pi
+	def getVolume(self):
+		self.volume = ((4 * math.pi * radius * radius)/3)
+		return volume
+	def getSurfaceArea(self):
+		self.sArea = (4 * math.pi * radius * radius)
+		return sArea
