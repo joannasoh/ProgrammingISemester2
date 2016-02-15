a7fde16a2802f2d1ef0bb1db2b0861782a428f1e
Joanna Soh <joannahsoh@gmail.com>
2016-02-15 12:54:06 -0700

    Pyramid

---
+++ Term3/JSShapes/Pyramid.py
@@ -0,0 +1,11 @@
+class Pyramid():
+	
+	def __init__(self,length, breadth, height, base):
+		self.length = length
+		self.breadth = breadth
+		self.height = height
+		self.base = length * breadth
+	def getVolume(self):
+		self.volume = ((length * breadth * height)/3)
+		return volume 
+	
